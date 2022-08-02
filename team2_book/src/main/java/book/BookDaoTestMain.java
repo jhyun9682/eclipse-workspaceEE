5b817fa0b016dd4e6fb50abdeba1217eb74caac3
academyitwill <jhyun9682@naver.com>
2022-08-02 19:15:59 +0900

    ..

--- team2_book/src/main/java/book/BookDaoTestMain.java
+++ team2_book/src/main/java/book/BookDaoTestMain.java
@@ -1,13 +1,33 @@
 package book;
 
+import java.util.List;
+
 import book.BookDao;
 
 public class BookDaoTestMain {
 
 	public static void main(String[] args)throws Exception {
+		/*
 		BookDao bookDao=new BookDao();
 		System.out.println(bookDao.selectAll());
-		
+		//전체리스트
+				List<Book> bookA=bookDao.selectAll();
+				for(Book book: bookA) {
+					System.out.println(book);
+				}
+				
+				//제목 검색
+				System.out.println(bookDao.selectByName("마시멜로이야기"));
+						
+				//저자 검색
+				
+				
+				//카테고리 검색
+				List<Book> bookC=bookDao.selectByClass("요리");
+				for(Book book:bookC) {
+					System.out.println(book);
+				}
+		*/
 
 	}
 
--- user_product_cart_order_board/src/main/java/com/itwill/shop/product/test/ProductDaoTestMain.java
+++ user_product_cart_order_board/src/main/java/com/itwill/shop/product/test/ProductDaoTestMain.java
@@ -1,12 +1,16 @@
 package com.itwill.shop.product.test;
 
+
 import com.itwill.shop.product.ProductDao;
 
 public class ProductDaoTestMain {
 
 	public static void main(String[] args)throws Exception  {
 		ProductDao productDao=new ProductDao();
+	
 		System.out.println(productDao.selectAll());
+		
+		
 
 	}
 
